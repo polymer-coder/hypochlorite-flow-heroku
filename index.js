968e255df33b2f9750b1ea38e8b7cdc342a1ba1e
Simeon <developer.polymer@gmail.com>
2023-05-11 16:32:45 -0400

    added correct port for heroku

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
